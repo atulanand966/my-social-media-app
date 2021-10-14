0b88f2efb52dad96ee269431583c7c37f8c85692
atulanand966 <atulanand966@gmail.com>
2021-10-14 18:25:34 +0530

    CRUD operations added successfully

--- src/main/java/com/socialmedia/mysocialmediaapp/controllers/UserJPARestController.java
+++ src/main/java/com/socialmedia/mysocialmediaapp/controllers/UserJPARestController.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +41,29 @@ public class UserJPARestController {
 			throw new UserNotFoundException("User with id:"+id+" not found");
 		}
 		return user;
+	}
+	
+	@PostMapping("/users")
+	public User save(@RequestBody User user) {
+		user.setId(0);
+		userJpaDAO.saveAndFlush(user);
+		return user;
+	}
+	
+	@PutMapping("/users")
+	public User update(@RequestBody User user) {
+		
+		Optional<User> optionalUser = userJpaDAO.findById(user.getId());
+		if(!optionalUser.isPresent()) {
+			throw new UserNotFoundException("User with id:"+user.getId()+" not found");
+		}
+		userJpaDAO.saveAndFlush(user);
+		return user;
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public String deleteById(@PathVariable int id) {
+		userJpaDAO.deleteById(id);
+		return "User deleted successfully.";
 	}
 }
